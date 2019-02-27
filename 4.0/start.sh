958b7b73d0d8f69f770d7761d41c610bebc508d5
Guox <33855684+dazuimao1990@users.noreply.github.com>
2019-02-27 17:02:17 +0800

    [UPDATE] change conf create from env2config to env2file

--- 4.0/start.sh
+++ 4.0/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "set redis config"
 chmod +x /usr/bin/env2config
-env2config config -f /usr/local/etc/redis/redis.conf
+env2file create --path /usr/local/etc/redis/redis.conf --format redis
 redis-server /usr/local/etc/redis/redis.conf
