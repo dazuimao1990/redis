362dc818c8d59a87316649113760ef516a024825
Guox <33855684+dazuimao1990@users.noreply.github.com>
2019-02-27 17:02:43 +0800

    Update start.sh

--- 4.0/start.sh
+++ 4.0/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "set redis config"
-chmod +x /usr/bin/env2config
+chmod +x /usr/bin/env2file
 env2file create --path /usr/local/etc/redis/redis.conf --format redis
 redis-server /usr/local/etc/redis/redis.conf
