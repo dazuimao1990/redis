3523a8a20d973663f68f171181ff61d499af9f03
guox <guox@goodrain.com>
2019-02-28 10:04:38 +0800

    no message

--- 4.0/start.sh
+++ 4.0/start.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 [[ $DEBUG ]] && set -x 
 echo "set redis config"
-fuser /usr/bin/env2file
 chmod +x /usr/bin/env2file
-fuser /usr/bin/env2file
 sleep 3
-fuser /usr/bin/env2file
 env2file create --path /usr/local/etc/redis/redis.conf --perm 0644 --format redis
 redis-server /usr/local/etc/redis/redis.conf
